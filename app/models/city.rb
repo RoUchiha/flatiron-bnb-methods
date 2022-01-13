20b8f4408c411fd01d0690f18a25ba369a61f1be
RoUchiha <swaggsikh@gmail.com>
2022-01-12 19:35:31 -0600

    commit tree 705224a803d23f0923e152ad4f4fc51ff81a046d

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,27 @@
 class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
+  has_many :reservations, :through => :listings 
+
+  include Reservable
+
+  def city_openings(start, ending)
+    start_date = Date.parse(start.to_s)
+    end_date = Date.parse(ending.to_s)
+    openings = []
+    self.listings.each do |n|
+      n.reservations.each do |r|
+      if r.checkin >= start_date && r.checkin <= end_date 
+        openings << n
+      end
+    end
+    end
+    return openings
+  end
+
+  
+  
+
 
 end
 
